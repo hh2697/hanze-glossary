b4a96cc73d01a2a60a87db338334313a7abcede7
hh2697 <31945278+hh2697@users.noreply.github.com>
2025-10-30 16:56:40 -0700

    Add files via upload

---
+++ fb-config.js
@@ -0,0 +1,3 @@
+// Auto-loaded Firebase config for all devices
+window.FB_CONFIG = {"apiKey": "AIzaSyA5OKWctsxpgVLmZC4xWeiLslVVxfesznI", "authDomain": "hanze-market-glossary.firebaseapp.com", "projectId": "hanze-market-glossary", "storageBucket": "hanze-market-glossary.firebasestorage.app", "messagingSenderId": "203124624833", "appId": "1:203124624833:web:845c4c51fa539405d6f340"};
+window.FB_PATH = "glossaries/hanze";
